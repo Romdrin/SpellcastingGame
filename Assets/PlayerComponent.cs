5e17bffd8aa5da8ee49e0aa65a375933c6f51262
Romdrin <loislopezvaldes@gmail.com>
2019-02-23 00:36:06 +0100

    Implmentación parcial animaciones y script de movimiento

---
+++ Assets/PlayerComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponent : MonoBehaviour
+{
+
+    private Animator animator;
+    private Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        //transform.position += new Vector3(horizontal, vertical, 1);
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetTrigger("walkAnimation");
+            animator.SetBoolean("idleAnimation", false);
+        }
+        
+    }
+}
