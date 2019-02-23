36e27517111e4ecba2ae4c77b96f0cfb692bfd68
Romdrin <loislopezvaldes@gmail.com>
2019-02-23 12:29:41 +0100

    Mejorado script de movimiento

--- Assets/PlayerComponent.cs
+++ Assets/PlayerComponent.cs
@@ -4,28 +4,59 @@ using UnityEngine;
 
 public class PlayerComponent : MonoBehaviour
 {
-
+    public float speed;
     private Animator animator;
-    private Transform transform;
+    //private Transform transform;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        transform = GetComponent<Transform>();
+        //transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        //transform.position += new Vector3(horizontal, vertical, 1);
-        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        Vector2 moveDirection = Vector2.zero;
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        if(horizontal > 0)
         {
+            moveDirection.x = 1;
             animator.SetTrigger("walkAnimation");
-            animator.SetBoolean("idleAnimation", false);
+            //animator.SetInteger("Direction", 2);
         }
+        if (horizontal < 0)
+        {
+            moveDirection.x = -1;
+            animator.SetTrigger("walkAnimation");
+            //animator.SetInteger("Direction", 0);
+        }
+        if (vertical > 0)
+        {
+            moveDirection.y = 1;
+            animator.SetTrigger("walkAnimation");
+            //animator.SetInteger("Direction", 1);
+        }
+        if (vertical < 0)
+        {
+            moveDirection.y = -1;
+            animator.SetTrigger("walkAnimation");
+            //animator.SetInteger("Direction", 3);
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if(horizontal != 0 || vertical != 0)
+            {
+                horizontal = 0;
+                vertical = 0;
+                animator.SetBool("wasWalking", true);
+            }
+            animator.SetTrigger("attackAnimation");
+            
+        }
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
         
     }
 }
